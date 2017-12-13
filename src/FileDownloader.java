2dca80d993f499b57b67c8a538c526e056c78f6d
MicahMSN <jpackit@gmail.com>
2017-12-14 00:30:01 +0300

    Initial commit

---
+++ src/FileDownloader.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.file.Files;
+
+public class FileDownloader {
+
+    public FileDownloader(String from, String to) {
+
+        try {
+            OutputStream outputStream = null;
+            URL url = new URL(from);
+            URLConnection connection = url.openConnection();
+            InputStream inputStream = connection.getInputStream();
+            File file = new File(to);
+            Files.copy(inputStream, file.toPath());
+        } catch (IOException e){
+
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new FileDownloader("https://www.google.com/images/srpr/logo11w.png", "C:\\google1.jpg");
+    }
+}
